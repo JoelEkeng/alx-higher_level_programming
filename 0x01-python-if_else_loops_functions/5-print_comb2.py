3813e1280897f416ed0667fd6f86a7a5a6321589
JoelEkeng <101402168+JoelEkeng@users.noreply.github.com>
2022-06-29 11:41:21 +0100

    print comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
-!/usr/bin/python3
 for i in range (-1,100):
-    if i in range(-1,10):
-        print (f"0{i},")
+    if i in range(0,10):
+        print (f"0{i},", end="")
     if i in range (10,100):
-        print (f"{a},")
+        print (f"{i},", end="")
